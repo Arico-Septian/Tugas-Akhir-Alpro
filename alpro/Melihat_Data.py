9efb47b14336e67f695f88f51c53357e69e4cfef
Arico Septian <aricoseptian5@gmail.com>
2023-01-27 08:44:14 +0700

    Melihat_Data

--- alpro/Melihat_Data.py
+++ alpro/Melihat_Data.py
@@ -3,27 +3,27 @@ listType = ["Galaxy a20", "Reno 8", "Xr"]
 listHarga = [2500000,3500000,4500000]
 
 def lihatData():
-    print("----------------------------------------------------")
-    print("|                     Daftar HP                    |")
-    print("----------------------------------------------------")
+    print("------------------------------------------------------------")
+    print("|><><><><><><><><><>< DAFTAR HANDPHONE ><><><><><><><><><><|")
+    print("------------------------------------------------------------")
     data = zip(listMerek, listType, listHarga)
-    print ("{:<5} {:<15} {:<15} {:<10}".format('No','Merk','Type','Harga'))
-    print("----------------------------------------------------")
+    print("| {:<5}| {:<15}| {:<15}| {:<16}|".format('No','Merk','Type','Harga'))
+    print("------------------------------------------------------------")
     for index, v in enumerate(data):
         merek, type, harga = v
-        print ("{:<5} {:<15} {:<15} {:<10}".format( index+1, merek, type, harga))
+        print ("| {:<5}| {:<15}| {:<15}| {:<16}|".format( index+1, merek, type, harga))
 
 listMenu = ["1.Menambah Data", "2.Menampilkan Data", "3.Mengubah Data" ,"4.Menghapus Data"]
 
 while True :
-    print("-----------------------")
-    print("|    Daftar Menu Hp : |")
-    print("-----------------------")
+    print("------------------------")
+    print("|    Daftar Menu Hp :  |")
+    print("------------------------")
     for x in listMenu :
         print(x)
-    print("-----------------------")
-    print("|     Pilih Menu   :  |")
-    print("-----------------------")
+    print("----------------------")
+    print("|    Pilih Menu   :  |")
+    print("----------------------")
     menu =int(input())
     if menu == 1 :
         tambahData()
@@ -35,4 +35,4 @@ while True :
         hapusData()
     else :
         break
-    print("----------------------------------------------------")
+    print("------------------------------------------------------------")
