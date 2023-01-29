1c413ed9edce8495444e7022f0482994a15d5c25
Arico Septian <aricoseptian5@gmail.com>
2023-01-29 08:14:28 +0700

    Melihat_Data

--- alpro/Melihat_Data.py
+++ alpro/Melihat_Data.py
@@ -1,7 +1,3 @@
-listMerek = ["samsung", "oppo", "iphone"]
-listType = ["Galaxy a20", "Reno 8", "Xr"]
-listHarga = [2500000,3500000,4500000]
-
 def lihatData():
     print("------------------------------------------------------------")
     print("|><><><><><><><><><>< DAFTAR HANDPHONE ><><><><><><><><><><|")
@@ -13,6 +9,25 @@ def lihatData():
         merek, type, harga = v
         print ("| {:<5}| {:<15}| {:<15}| {:<16}|".format( index+1, merek, type, harga))
 
+my_file = open("NamaList.txt", "r")
+count = 0
+
+listMerek = []
+listType = []
+listHarga = []
+
+while True:
+    count += 1  
+    data = my_file.readline()
+    if not data:
+        break
+    if count == 1 :
+        listMerek = data.rstrip().split(",")
+    elif count == 2 :
+        listType = data.rstrip().split(",")
+    elif count == 3 :
+        listHarga = data.rstrip().split(",")
+        
 listMenu = ["1.Menambah Data", "2.Menampilkan Data", "3.Mengubah Data" ,"4.Menghapus Data"]
 
 while True :
